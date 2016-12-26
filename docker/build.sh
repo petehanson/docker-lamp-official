a7b21d5bf50e0c25c4c760018301797606023023
Peter Hanson <peter@upandrunningsoftware.com>
2016-12-25 19:42:57 -0600

    updated build and compose files

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-docker build -f webserver/Dockerfile -t petehanson/webserver:latest ./webserver
+docker build -f nginx/Dockerfile -t petehanson/nginx:latest ./nginx
+docker build -f fpm/Dockerfile -t petehanson/fpm:latest ./fpm
+docker build -f mysql/Dockerfile -t petehanson/mysql:latest ./mysql
 docker build -f node/Dockerfile -t petehanson/node:latest ./node
