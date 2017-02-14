246d3a5210d0bfe0e8496ac0e784ebc2d56848b1
Peter Hanson <peter@upandrunningsoftware.com>
2017-02-13 22:23:30 -0600

    updated the templates to handle the cron container build

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ docker build -f docker/nginx/Dockerfile -t petehanson/nginx:latest ./docker/ngin
 docker build -f docker/fpm/Dockerfile -t petehanson/fpm:latest ./docker/fpm
 docker build -f docker/mysql/Dockerfile -t petehanson/mysql:latest ./docker/mysql
 docker build -f docker/node/Dockerfile -t petehanson/node:latest ./docker/node
+docker build -f docker/cron/Dockerfile -t petehanson/cron:latest ./docker/cron
--- docker/build-template.sh
+++ docker/build-template.sh
@@ -4,3 +4,4 @@ docker build -f docker/nginx/Dockerfile -t {image_namespace}/nginx:latest ./dock
 docker build -f docker/fpm/Dockerfile -t {image_namespace}/fpm:latest ./docker/fpm
 docker build -f docker/mysql/Dockerfile -t {image_namespace}/mysql:latest ./docker/mysql
 docker build -f docker/node/Dockerfile -t {image_namespace}/node:latest ./docker/node
+docker build -f docker/cron/Dockerfile -t {image_namespace}/cron:latest ./docker/cron
