6f1e89ff95a06c09df6872fd211c3e53100999f6
Peter Hanson <peter@upandrunningsoftware.com>
2017-03-19 16:40:26 -0500

    updated the develop script and added a bit more boilerplate code for testing

---
+++ code/test/SampleTest.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once(dirname(__FILE__) . "/../vendor/autoload.php");
+
+class SampleTest extends \PHPUnit_Framework_TestCase {
+
+    public function testSample1() {
+        $this->assertTrue(true);
+    }
+}
